d6968eddb0655a89751fef240d27ba485db28b21
Andrii <A.I.Khmelovsky@gmail.com>
2019-09-03 15:40:07 +0300

    Name update event

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMsg = document.querySelector('.update-msg');
 
 newChatForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -10,7 +12,22 @@ newChatForm.addEventListener('submit', event => {
             newChatForm.reset()
         })
         .catch(err => console.log(err));
-})
+});
+
+newNameForm.addEventListener('submit', event =>{
+    event.preventDefault();
+    const userName = newNameForm.name.value.trim();
+
+    chatroom.updateName(userName);
+
+    newNameForm.reset();
+      
+    updateMsg.innerText = `Your name was updated to ${userName}`;
+
+    setTimeout(() => {
+        updateMsg.innerText = '';
+    }, 3000);
+});
 
 // class instances
 const chatUI = new ChatUI(chatList);
